192601ed4c155fa18e209872472e8b81c9efe554
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-08-09 17:41:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		this.name : name;
+		this.age: age;
+	}
+}
+get name(){
+	this.name 
+}
+set age(){
+	this.age: age;
+	
+}
+class Student extends Person {
+	study(){
+		console.log(`${this.name} is studying`)
+	}
+}
 
-class Student extends Person {}
+class Teacher extends Person {
+	teach(){
+		console.log(`${this.name} is teaching`);
+	}
+}
+const person = new person("john", 13);
+const teacher = new teacher("akhil", 27);
+teacher.teach();
 
-class Teacher extends Person {}
+const student = new student('swati', 18);
+student.study();
+const student1 = new student('abha', 20);
+
 
 // Do not change the code below this line
 window.Person = Person;
